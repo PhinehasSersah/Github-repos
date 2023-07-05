673cf3778e066712ca3ba204113ad62547ef0678
phinehas <phine.amd@gmail.com>
2023-07-05 07:55:36 +0000

    Hit the github API endpoint

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./search.css";
 import { ShowSearchProps } from "../../App";
 
@@ -6,6 +6,36 @@ const SearchComponent = ({
   setShowSearch,
   showSearch,
 }: ShowSearchProps): React.ReactElement => {
+  const [githubUserName, setgithubUserName] = useState<string>("");
+
+  const [repositories, setrepositories] = useState([]);
+  console.log(repositories);
+
+  const fetchRepositories = async () => {
+    if (githubUserName === "") return;
+
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${githubUserName}/repos?type=all&sort=updated`,
+        {
+          headers: {
+            Accept: "application/vnd.github.v3+json",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      setrepositories(data);
+    } catch (error) {
+      throw new Error(`Error: ${error.message}`);
+    }
+  };
+
   return (
     <div className={`search__wrapper ${showSearch && "translate__search"}`}>
       <h2 className="search__headline">
@@ -13,6 +43,8 @@ const SearchComponent = ({
       </h2>
       <div className="input__wrapper">
         <input
+          value={githubUserName}
+          onChange={(event) => setgithubUserName(event.target.value)}
           className="input__field"
           type="search"
           placeholder="Enter github username"
@@ -20,7 +52,7 @@ const SearchComponent = ({
           autoComplete="off"
         />
         <div className="button__wrapper">
-          <button className="button">
+          <button onClick={fetchRepositories} className="button">
             <span>Search Repo's</span>
           </button>
         </div>
